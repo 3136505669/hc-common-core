1ae5f3667c3a95a20e5e0d99090ff5939bd91f49
lijinxi <3136505669@qq.com>
2017-02-07 15:01:01 +0800

    添加注釋

---
+++ src/main/java/com/hc/scm/common/App.java
@@ -0,0 +1,14 @@
+package com.hc.scm.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //牛牛天空
+    }
+}
